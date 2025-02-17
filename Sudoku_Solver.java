1a3bfc52b363d3fe51b3f254a7acd91b31a60ac0
Blober8000 <vascorafael.tavares+GitHub@gmail.com>
2025-02-17 10:34:13 +0000

    Replaced all 0s in board[][][0] with CELL_NUMBER for clarity

--- Sudoku_Solver.java
+++ Sudoku_Solver.java
@@ -8,7 +8,7 @@ public class Sudoku_Solver {
     public static Scanner scanner = new Scanner(System.in);
     public static int[][] order = {{0, 0}, {0, 1}, {0, 2}, {1, 0}, {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}, {0, 3}, {0, 4}, {0, 5}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {0, 6}, {0, 7}, {0, 8}, {1, 6}, {1, 7}, {1, 8}, {2, 6}, {2, 7}, {2, 8}, {3, 0}, {3, 1}, {3, 2}, {4, 0}, {4, 1}, {4, 2}, {5, 0}, {5, 1}, {5, 2}, {3, 3}, {3, 4}, {3, 5}, {4, 3}, {4, 4}, {4, 5}, {5, 3}, {5, 4}, {5, 5}, {3, 6}, {3, 7}, {3, 8}, {4, 6}, {4, 7}, {4, 8}, {5, 6}, {5, 7}, {5, 8}, {6, 0}, {6, 1}, {6, 2}, {7, 0}, {7, 1}, {7, 2}, {8, 0}, {8, 1}, {8, 2}, {6, 3}, {6, 4}, {6, 5}, {7, 3}, {7, 4}, {7, 5}, {8, 3}, {8, 4}, {8, 5}, {6, 6}, {6, 7}, {6, 8}, {7, 6}, {7, 7}, {7, 8}, {8, 6}, {8, 7}, {8, 8}};
     public static int[][] Square = {{0, 1, 2, 3, 4, 5, 6, 7, 8}, {9, 10, 11, 12, 13, 14, 15, 16, 17}, {18, 19, 20, 21, 22, 23, 24, 25, 26}, {27, 28, 29, 30, 31, 32, 33, 34, 35}, {36, 37, 38, 39, 40, 41, 42, 43, 44}, {45, 46, 47, 48, 49, 50, 51, 52, 53}, {54, 55, 56, 57, 58, 59, 60, 61, 62}, {63, 64, 65, 66, 67, 68, 69, 70, 71}, {72, 73, 74, 75, 76, 77, 78, 79, 80}};
-
+    public static final int CELL_NUMBER = 0;
 
     public static void main(String[] args) {
         if (!Arrays.equals(args, new String[]{})) {
@@ -58,13 +58,13 @@ public class Sudoku_Solver {
         for (int i = 0; i < 9; i++) {
             System.out.printf("%s%d%s ", red, i + 1, reset);
             for (int j = 0; j < 9; j++) {
-                if (board[i][j][0] == 0) {
+                if (board[i][j][CELL_NUMBER] == 0) {
                     System.out.printf("%s ", "*");
                 } else {
                     if (board[i][j][1] == board[i][j][2] && board[i][j][2] == board[i][j][3] && board[i][j][3] == board[i][j][4] && board[i][j][4] == board[i][j][5] && board[i][j][5] == board[i][j][6] && board[i][j][6] == board[i][j][7] && board[i][j][7] == board[i][j][8] && board[i][j][8] == board[i][j][9]) {
-                        System.out.printf("%d ", board[i][j][0]);
+                        System.out.printf("%d ", board[i][j][CELL_NUMBER]);
                     } else {
-                        System.out.printf("%s%d%s ", blue, board[i][j][0], reset);
+                        System.out.printf("%s%d%s ", blue, board[i][j][CELL_NUMBER], reset);
                     }
                 }
             }
@@ -133,7 +133,7 @@ public class Sudoku_Solver {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 for (int k = 1; k < 10; k++) {
-                    if (board[i][j][0] == 0) {
+                    if (board[i][j][CELL_NUMBER] == 0) {
                         board[i][j][k] = k;
                     }
                 }
@@ -152,16 +152,16 @@ public class Sudoku_Solver {
             for (int i = 0; i < 81; i++) {
                 x = order[i][0];
                 y = order[i][1];
-                if (board[x][y][0] == 0) {
+                if (board[x][y][CELL_NUMBER] == 0) {
                     UpdateNumber(i);
                     for (int j = 0; j < 9; j++) {
-                        current = board[x][j][0];
+                        current = board[x][j][CELL_NUMBER];
                         if (current != 0) {
                             board[x][y][current] = 0;
                         }
                     }
                     for (int j = 0; j < 9; j++) {
-                        current = board[j][y][0];
+                        current = board[j][y][CELL_NUMBER];
                         if (current != 0) {
                             board[x][y][current] = 0;
                         }
@@ -172,7 +172,7 @@ public class Sudoku_Solver {
                     for (int sq : Square[square]) {
                         xsq = order[sq][0];
                         ysq = order[sq][1];
-                        current = board[xsq][ysq][0];
+                        current = board[xsq][ysq][CELL_NUMBER];
                         if (current != 0) {
                             board[x][y][current] = 0;
                             countsq++;
@@ -183,7 +183,7 @@ public class Sudoku_Solver {
                         for (int sq : Square[square]) {
                             xsq = order[sq][0];
                             ysq = order[sq][1];
-                            currentsq = board[xsq][ysq][0];
+                            currentsq = board[xsq][ysq][CELL_NUMBER];
                             if (currentsq != 0) {
                                 acc += currentsq;
                             } else {
@@ -206,7 +206,7 @@ public class Sudoku_Solver {
                                     if (board[x][k][j] != 0) {
                                         board[x][k][0] = j;
                                         for (int l = 1; l < 10; l++) {
-                                            if ((board[x][k][l] != board[x][k][0])) {
+                                            if ((board[x][k][l] != board[x][k][CELL_NUMBER])) {
                                                 board[x][k][l] = 0;
                                             }
                                         }
@@ -226,9 +226,9 @@ public class Sudoku_Solver {
                             if (counter == 1) {
                                 for (int k = 0; k < 9; k++) {
                                     if (board[k][y][j] != 0) {
-                                        board[k][y][0] = j;
+                                        board[k][y][CELL_NUMBER] = j;
                                         for (int l = 1; l < 10; l++) {
-                                            if ((board[k][y][l] != board[k][y][0])) {
+                                            if ((board[k][y][l] != board[k][y][CELL_NUMBER])) {
                                                 board[k][y][l] = 0;
                                             }
                                         }
@@ -249,9 +249,9 @@ public class Sudoku_Solver {
                             if (counter == 1) {
                                 for (int k : Square[square]) {
                                     if (board[order[k][0]][order[k][1]][j] != 0) {
-                                        board[order[k][0]][order[k][1]][0] = j;
+                                        board[order[k][0]][order[k][1]][CELL_NUMBER] = j;
                                         for (int l = 1; l < 10; l++) {
-                                            if (board[order[k][0]][order[k][1]][l] != board[order[k][0]][order[k][1]][0]) {
+                                            if (board[order[k][0]][order[k][1]][l] != board[order[k][0]][order[k][1]][CELL_NUMBER]) {
                                                 board[order[k][0]][order[k][1]][l] = 0;
                                             }
                                         }
@@ -268,8 +268,6 @@ public class Sudoku_Solver {
                 break;
             }
             DrawBoard();
-//            System.out.println(Arrays.toString(board[2][0]));
-//            scanner.nextLine();
             if (protection >= 100) {
                 while (true) {
                     int[] in;
@@ -299,7 +297,7 @@ public class Sudoku_Solver {
         if (cont == 8) {
             for (int j = 1; j < 10; j++) {
                 if ((board[x][y][j] != 0)) {
-                    board[x][y][0] = j;
+                    board[x][y][CELL_NUMBER] = j;
                 }
             }
         }
@@ -310,7 +308,7 @@ public class Sudoku_Solver {
         for (int i = 0; i < 81; i++) {
             x = order[i][0];
             y = order[i][1];
-            board[x][y][0] = Integer.parseInt(args[i]);
+            board[x][y][CELL_NUMBER] = Integer.parseInt(args[i]);
         }
     }
 }
